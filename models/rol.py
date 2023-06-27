13b00e89480fd047c5180386c4c696005a14ab91
unknown <paul.huaroc.r@gmail.com>
2023-06-26 20:17:42 -0500

    id_rol descomentado

--- models/rol.py
+++ models/rol.py
@@ -10,6 +10,6 @@ class Rol(db.Model):
     id_rol=db.Column(db.Integer, primary_key=True)
     descripcion=db.Column(db.String(60))
     
-    def __init__(self,descripcion):
-        #self.id_rol=id_rol
+    def __init__(self,id_rol,descripcion):
+        self.id_rol=id_rol
         self.descripcion=descripcion
